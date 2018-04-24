a4c11c291c3f31bf658f19be65102607e747a952
Jeff LaJoie <jlajoie@edx.org>
2018-04-24 09:49:50 -0400

    remove line

--- openedx/core/djangoapps/credentials/settings/aws.py
+++ openedx/core/djangoapps/credentials/settings/aws.py
@@ -3,8 +3,6 @@
 
 def plugin_settings(settings):
     # Credentials Settings
-    settings.ACE_ROUTING_KEY = settings.ENV_TOKENS.get('ACE_ROUTING_KEY', settings.ACE_ROUTING_KEY)
-
     settings.CREDENTIALS_INTERNAL_SERVICE_URL = settings.ENV_TOKENS.get(
         'CREDENTIALS_INTERNAL_SERVICE_URL', settings.CREDENTIALS_INTERNAL_SERVICE_URL
     )
